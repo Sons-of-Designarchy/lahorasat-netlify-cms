16899492dbed55f4a05c3d12bd74005a9ba8f825
Daniel Pliego <danielpliego@gmail.com>
2023-04-03 18:47:03 -0600

    vacante gerente de contabilidad

---
+++ src/pages/vacante-contabilidad.js
@@ -0,0 +1,113 @@
+import * as React from "react";
+import Layout from "../components/page-layout";
+// markup
+const AsesoriasPage = () => {
+  return (
+    <>
+      <Layout title="Vacante Contador" hideSubscribe>
+        <div className="container my-5">
+          <h1>Gerente de contabilidad</h1>
+          <p>
+            Estamos buscando un profesional experimentado en contabilidad para
+            unirse a nuestro equipo como líder del área contable de nuestro
+            proyecto. Este rol jugará un papel crítico en la dirección de
+            nuestro proyecto y en impulsar su éxito.
+          </p>
+          <h4>Responsabilidades</h4>
+          <ul>
+            <li>
+              Supervisar la operación diaria del proyecto y empujar las metas
+              del mes
+            </li>
+            <li>
+              Colaborar con el equipo de marketing para generar contenido para
+              redes sociales
+            </li>
+            <li>Impartir cursos sobre el SAT a grupos online y presencial.</li>
+            <li>Resolver dudas fiscales que pasen en el chat de soporte.</li>
+            <li>
+              Desarrollar y ejecutar una estrategia de impuestos integral que se
+              alinee con nuestros objetivos empresariales y cumpla con todas las
+              regulaciones y leyes pertinentes.
+            </li>
+            <li>
+              Administrar todos los aspectos de la preparación de impuestos del
+              proyecto.
+            </li>
+            <li>
+              Colaborar con otros miembros del equipo para identificar áreas de
+              mejora de procesos y desarrollar soluciones para mejorar nuestra
+              eficiencia y efectividad general.
+            </li>
+          </ul>
+          <h4>Requisitos</h4>
+          <ul>
+            <li>Licenciatura en contabilidad.</li>
+            <li>
+              Al menos 3 años de experiencia en la preparación de impuestos y
+              contabilidad.
+            </li>
+            <li>
+              Fuertes habilidades analíticas, de resolución de problemas y de
+              comunicación.
+            </li>
+            <li>
+              Habilidad probada para administrar proyectos complejos y obtener
+              resultados.
+            </li>
+            <li>
+              Se valora la experiencia trabajando en un entorno ágil o con
+              empresas en etapas tempranas.
+            </li>
+          </ul>
+          <h4>Nice-to-haves</h4>
+          <ul>
+            <li>Experiencia en atención al cliente</li>
+            <li>Experiencia de redacción y/o pedagogía</li>
+            <li>Experiencia con prensa/entrevistas/PR.</li>
+          </ul>
+          <h4>¿Qué te ofrecemos?</h4>
+          <ul>
+            <li>
+              Paquete de compensación competitivo, que incluye participación
+              accionaria en la empresa.
+            </li>
+            <li>2 días de vacaciones al mes.</li>
+            <li>Trabajo hibrido o full remoto según tu ubicación.</li>
+            <li>
+              Espacio de trabajo disponible en estudio en la Narvarte (CDMX).
+            </li>
+            <li>
+              Pago de materiales para tu desarrollo profesional a través de
+              conferencias, cursos, libros, etc y artículos de oficina
+              necesarios.
+            </li>
+          </ul>
+          <h4>¿Por qué trabajar con nosotros?</h4>
+          <p>
+            Somos la comunidad de educación fiscal más grande. Tenemos año y
+            medio en el mercado y en ese tiempo hemos logrado ayudar a más de 50
+            mil personas a aprender de forma sencilla cómo funciona el SAT,
+            nuestra misión es ayudar a las nuevas generaciones con educación
+            fiscal divertida y fácil de entender con un enfoque de trabajo ágil,
+            creativo y socialmente responsable.
+          </p>
+          <p>
+            La Hora Sat es parte de Soda, un estudio donde desarrollamos y
+            colaboramos con proyectos creativos que buscan generar un impacto
+            social. Soda está formado por un equipo multidisciplinario y modular
+            de creativos, diseñadores y programadores que colabora para brindar
+            soluciones integrales que van desde el diseño de marca, manejo de
+            redes sociales, producción de eventos y desarrollo web.
+          </p>
+          <h4>¿Cómo aplicar?</h4>
+          Envía un correo a <a href="mailto:lahorasat@gmail.com">lahorasat@gmail.com</a> con el asunto "Gerente de
+          contabilidad" donde incluyas tu CV actualizado y un video de menos de 3mins donde nos
+          cuentes por qué quieres ser parte del equipo.
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default AsesoriasPage;
