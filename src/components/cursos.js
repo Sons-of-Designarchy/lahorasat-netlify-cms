21cf5a2e5ef7f0d1143a5f5c34b2d87128a7d28b
Daniel Pliego <danielpliego@gmail.com>
2023-05-23 13:03:49 -0600

    remove unused stuff

--- src/components/cursos.js
+++ src/components/cursos.js
@@ -2,13 +2,8 @@
 import React from "react";
 import curso from "../img/cursos/curso-thumbnail-video.jpg";
 import taller from "../img/cursos/curso-thumbnail-taller.jpg";
-import BannerCursos from "./banner-cursos";
-import BannerTaller from "./banner-taller";
-import BannerTallerBasico from "./banner-taller-basico";
 
 const Cursos = () => {
-  const [openModal, setOpenModal] = React.useState("");
-
   return (
     <>
       <div className="section section-purple">
@@ -74,98 +69,8 @@ const Cursos = () => {
                     </div>
                   </a>
                 </div>
-                {/* 
-                <div className="col-lg-4 mb-3">
-                  <a
-                    className="course-card rounded"
-                    href="/taller-hacks-del-sat"
-                  >
-                    <img src={taller} className="img-fluid" />
-                    <div className="course-content">
-                      <div className="d-flex mb-2">
-                        <div className="label label-primary me-2">
-                          TALLER EN ZOOM
-                        </div>
-                        <div className="label label-primary me-2">$799 MXN</div>
-                      </div>
-                      <h4 className="mb-1 text-secondary">
-                        Hacks del sat para Freelancers y RIF's
-                      </h4>
-                      <p className="font-sm text-secondary-l">
-                        Aquí aprenderás de forma práctica cómo navegar a través del portal del SAT, cómo declarar, facturar y dónde encontrar tus documentos fiscales más importantes. 
-                      </p>
-                    </div>
-                    <div className="p-3">
-                      <a className="btn btn-secondary btn-block disabled">Próximamente →</a>
-                    </div>
-                  </a>
-                </div> */}
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        className={
-          openModal == "video" ? "section-modal open" : "section-modal"
-        }
-      >
-        <div
-          className="section-modal-backdrop"
-          onClick={() => setOpenModal("")}
-        ></div>
-        <div className="section-modal-content">
-          <button
-            onClick={() => setOpenModal("")}
-            className="section-modal-close"
-          >
-            X
-          </button>
-          <div className="section-purple">
-            <BannerCursos />
-          </div>
-        </div>
-      </div>
-      <div
-        className={
-          openModal == "taller" ? "section-modal open" : "section-modal"
-        }
-      >
-        <div
-          className="section-modal-backdrop"
-          onClick={() => setOpenModal("")}
-        ></div>
-        <div className="section-modal-content">
-          <button
-            onClick={() => setOpenModal("")}
-            className="section-modal-close"
-          >
-            X
-          </button>
-          <div className="section-purple">
-            <BannerTaller />
-          </div>
-        </div>
-      </div>
-      <div
-        className={
-          openModal == "taller-basico" ? "section-modal open" : "section-modal"
-        }
-      >
-        <div
-          className="section-modal-backdrop"
-          onClick={() => setOpenModal("")}
-        ></div>
-        <div className="section-modal-content">
-          <button
-            onClick={() => setOpenModal("")}
-            className="section-modal-close"
-          >
-            X
-          </button>
-          <div className="section-purple">
-            <BannerTallerBasico />
           </div>
         </div>
       </div>
